81df341a09f9fbb90b28aba496be0afdab675a42
fuginator <fugier@gmail.com>
2014-06-18 17:03:20 -0700

    Add UI Elements, COM port settings

--- ChainsawProtocolTest/ChainsawProtocolTest/MainForm.Designer.cs
+++ ChainsawProtocolTest/ChainsawProtocolTest/MainForm.Designer.cs
@@ -28,13 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.cboPort = new System.Windows.Forms.ComboBox();
+            this.cboBaudRate = new System.Windows.Forms.ComboBox();
+            this.cboParity = new System.Windows.Forms.ComboBox();
+            this.cboDataBits = new System.Windows.Forms.ComboBox();
+            this.cboStopBits = new System.Windows.Forms.ComboBox();
+            this.cboHandshake = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
+            // 
+            // cboPort
+            // 
+            this.cboPort.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboPort.FormattingEnabled = true;
+            this.cboPort.Location = new System.Drawing.Point(12, 12);
+            this.cboPort.Name = "cboPort";
+            this.cboPort.Size = new System.Drawing.Size(121, 21);
+            this.cboPort.TabIndex = 0;
+            // 
+            // cboBaudRate
+            // 
+            this.cboBaudRate.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboBaudRate.FormattingEnabled = true;
+            this.cboBaudRate.Location = new System.Drawing.Point(139, 12);
+            this.cboBaudRate.Name = "cboBaudRate";
+            this.cboBaudRate.Size = new System.Drawing.Size(121, 21);
+            this.cboBaudRate.TabIndex = 1;
+            // 
+            // cboParity
+            // 
+            this.cboParity.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboParity.FormattingEnabled = true;
+            this.cboParity.Location = new System.Drawing.Point(266, 12);
+            this.cboParity.Name = "cboParity";
+            this.cboParity.Size = new System.Drawing.Size(121, 21);
+            this.cboParity.TabIndex = 2;
+            // 
+            // cboDataBits
+            // 
+            this.cboDataBits.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboDataBits.FormattingEnabled = true;
+            this.cboDataBits.Location = new System.Drawing.Point(393, 12);
+            this.cboDataBits.Name = "cboDataBits";
+            this.cboDataBits.Size = new System.Drawing.Size(121, 21);
+            this.cboDataBits.TabIndex = 3;
+            // 
+            // cboStopBits
+            // 
+            this.cboStopBits.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboStopBits.FormattingEnabled = true;
+            this.cboStopBits.Location = new System.Drawing.Point(520, 12);
+            this.cboStopBits.Name = "cboStopBits";
+            this.cboStopBits.Size = new System.Drawing.Size(121, 21);
+            this.cboStopBits.TabIndex = 4;
+            // 
+            // cboHandshake
+            // 
+            this.cboHandshake.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboHandshake.FormattingEnabled = true;
+            this.cboHandshake.Location = new System.Drawing.Point(647, 12);
+            this.cboHandshake.Name = "cboHandshake";
+            this.cboHandshake.Size = new System.Drawing.Size(121, 21);
+            this.cboHandshake.TabIndex = 5;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(601, 462);
+            this.ClientSize = new System.Drawing.Size(787, 462);
+            this.Controls.Add(this.cboHandshake);
+            this.Controls.Add(this.cboStopBits);
+            this.Controls.Add(this.cboDataBits);
+            this.Controls.Add(this.cboParity);
+            this.Controls.Add(this.cboBaudRate);
+            this.Controls.Add(this.cboPort);
             this.Name = "MainForm";
             this.Text = "Chainsaw Protocol Test";
             this.Load += new System.EventHandler(this.MainForm_Load);
@@ -43,6 +109,13 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox cboPort;
+        private System.Windows.Forms.ComboBox cboBaudRate;
+        private System.Windows.Forms.ComboBox cboParity;
+        private System.Windows.Forms.ComboBox cboDataBits;
+        private System.Windows.Forms.ComboBox cboStopBits;
+        private System.Windows.Forms.ComboBox cboHandshake;
     }
 }
 
--- ChainsawProtocolTest/ChainsawProtocolTest/MainForm.cs
+++ ChainsawProtocolTest/ChainsawProtocolTest/MainForm.cs
@@ -7,14 +7,62 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO.Ports;
 
 namespace ChainsawProtocolTest
 {
     public partial class MainForm : Form
     {
+        SerialPort sPort;
+
         public MainForm()
         {
-            InitializeComponent();
+            InitializeComponent();      // required
+            Console.WriteLine("Chainsaw Protocol Test");
+            initUI();
+        }
+
+        /// <summary>
+        /// Load valid values for the user interface
+        /// </summary>
+        private void initUI()
+        {
+            // Nifty bit to use later
+            // IEnumerable<string> ports = SerialPort.GetPortNames().ToList();
+            // ports = ports.Select(e => e.Replace("COM", "Communications Port "));
+
+            cboPort.DataSource = SerialPort.GetPortNames();
+            cboPort.SelectedIndex = cboPort.Items.Count - 1;
+
+            cboBaudRate.DataSource = new int[] { 4800, 9600, 19200, 38400, 57600, 115200, 130400 };
+            cboBaudRate.SelectedIndex = 1;   // default to 9600 baud
+
+            // Read with: (Parity)Enum.Parse(typeof(Parity), parity, true)
+            cboParity.DataSource = new string[]
+            { 
+                Parity.None.ToString(),
+                Parity.Even.ToString(),
+                Parity.Odd.ToString(), 
+                Parity.Space.ToString(),
+                Parity.Mark.ToString()
+            };
+
+            cboDataBits.DataSource = new int[] { 5, 6, 7, 8, 9 };
+            cboDataBits.SelectedIndex = 3;   // default to 8 data bits
+
+            cboStopBits.DataSource = new string[] {
+                StopBits.None.ToString(),
+                StopBits.One.ToString(),
+                StopBits.OnePointFive.ToString(),
+                StopBits.Two.ToString()
+            };
+
+            cboHandshake.DataSource = new string[] {
+                Handshake.None.ToString(),
+                Handshake.RequestToSend.ToString(),
+                Handshake.RequestToSendXOnXOff.ToString(),
+                Handshake.XOnXOff.ToString()
+            };
         }
 
         private void MainForm_Load(object sender, EventArgs e)
